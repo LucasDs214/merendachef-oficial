02d6d03171c42e08024a146847b69ac3b1ee1e43
LucasDs214 <153468040+LucasDs214@users.noreply.github.com>
2026-05-11 15:19:21 -0300

    Add telefone masking function

--- frontend/src/utils/masks.ts
+++ frontend/src/utils/masks.ts
@@ -6,3 +6,11 @@ export function maskCpf(value: string): string {
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1');
 }
+
+export function maskTelefone(value: string): string {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+}
